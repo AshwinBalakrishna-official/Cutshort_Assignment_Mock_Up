42eaa0b0102d1e41dd2f187507d39d1d3211d2e1
ashwinbalakrishna-official <ashwinbkrish@gmail.com>
2022-08-09 20:32:50 +0530

    added form button

--- src/components/ProgressBtn/ProgressBtn.jsx
+++ src/components/ProgressBtn/ProgressBtn.jsx
@@ -6,7 +6,7 @@ const ProgressBtn = (props) => {
       <>
       
         <div>
-              <button className='progress-btn' onClick={() => { props.pg<3 && props.choosePg(props.pg + 1) }}>{props.pg <3 ? "Create Workspace" : "Launch Eden"} </button>  
+              <button className='progress-btn' onClick={() => { props.pg<4 && props.choosePg(props.pg + 1) }}>{props.pg <4 ? "Create Workspace" : "Launch Eden"} </button>  
         </div>
       
       </>
